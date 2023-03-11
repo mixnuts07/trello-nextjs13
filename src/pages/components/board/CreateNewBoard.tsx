86cc9e0cce8fbac679e89f7cff324a2c8718092e
mixnuts07 <french0719@gmail.com>
2023-03-11 23:37:47 +0900

    borad ui

---
+++ src/pages/components/board/CreateNewBoard.tsx
@@ -0,0 +1,14 @@
+"use client";
+import { Button } from "@mui/material";
+import React from "react";
+
+export default function CreateNewBoard() {
+  return (
+    <div>
+      <Button variant="contained">Create Board...</Button>
+    </div>
+  );
+}
+
+// id
+const boards = [];
